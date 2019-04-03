d5db55f2b9ce5143cfc1051bc4680740b8e10e7c
MySideSafe <iff_leymarie.igor@hotmail.fr>
2019-04-03 20:52:56 +0200

    debut mise en page

--- js/App.js
+++ js/App.js
@@ -6,7 +6,6 @@ class App {
         this.initSlider();
         this.formulaire = new Formulaire(document.getElementById("formReservation"));
         this.initStations(this.map,this.formulaire); //Chargement des stations
-        //Si exite timer nop initReseration
       
         if(sessionStorage.getItem("timer") != null)
             {
@@ -42,10 +41,13 @@ class App {
     //Affichage de la liste des stations sur la map
     initStations(map,formulaire) {
         ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=73b8377b68cf91d454d51322942e64a69bf02c27", function (reponse) {
+            //On récupere les stations
             let stations = JSON.parse(reponse);
+            //pour chaque station on créer un marqueur sur la carte
             for (let uneStation of stations) {
                const station = new Station(uneStation.number, uneStation.position, uneStation.name, uneStation.address, uneStation.status, uneStation.available_bike_stands, uneStation.available_bikes);
                 station.ajouterMarqueur(map);
+                //au clic sur un marqueur on affiche les infos stations et le formulaire de réservation.
                 station.marqueur.addEventListener("click", function () {
                 station.chargerInfosStation(map);
                 formulaire.afficherForm(station);
@@ -58,18 +60,25 @@ class App {
     }
     
     initReservation(formulaire){
-       
+        //On écoute le submit du formulaire
         formulaire.formDom.addEventListener("submit", function (e) {
         e.preventDefault();
+        //création du client
         const client = new Client();
         client.recupererClient();
         client.enregistrerClientDansNavigateur();
         var date = new Date();
+        //création de la réservation
         const reservation = new Reservation(client,date);
         reservation.enregistrerReservation(client);
         reservation.afficherReservation(formulaire.station);
-            //verifier si timer existe
+        //Si un timer est déjà en cours on l'efface pour n'avoir qu'une seule réservation possible
+        if(sessionStorage.getItem("timer") != null){
+            sessionStorage.removeItem("timer");
+            
+        }
         var now = Date.now();
+        //création du timer
         var timer= new Timer(now, 0.5);
         timer.decompter();
         }
@@ -77,37 +86,27 @@ class App {
        
     }
     
-    //possible de creer un.json pour charger les slides
+    
     initSlider(){
         
-        
+        /*Version finale
         let slides=[];
+        //On récupere les infos des images dans un fichier JSON
         ajaxGet("http://igor-iff-leymarie.ovh/velo/slide.json", function (reponse) {
             let slidesJ = JSON.parse(reponse);
             console.log(slidesJ);
             for (let unSlide of slidesJ) {
                 var slide= new Slide(unSlide.image,unSlide.figcaption,unSlide.altImage);
                slides.push(slide);
-                console.log(slide);
-                console.log("b");
+
                 
             }
-
             }
         );
-        console.log(slides);
+        */
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        /*var slides =[];
+        var slides =[];
         let slide1 = new Slide("images/velov.jpg","Bienvenue dans l'application de réservation de vélo'v lyon","location velov");
         slides.push(slide1);
         let slide2 = new Slide("images/lyon_1.jpg","Choissisez votre station","ville de lyon");
@@ -116,9 +115,8 @@ class App {
         slides.push(slide3);
         let slide4 = new Slide("images/lyon_3.jpg","Votre Vélo'v est réservé pendant 20 minutes Après ce délais votre réservation sera automatique annulée","ville de lyon");
         slides.push(slide4);
-    */
+        //création du slider 
         var slider = new Slider("slideshow",slides);
-
         slider.listeners();
     }
 }
